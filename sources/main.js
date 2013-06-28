c160154f744bc009ab192b3ea159386cc5d0a280
thibault_eynard <eynard.thibault.isart@gmail.com>
2013-06-29 01:45:17 +0200

    base html + base main.js + qques samples

--- sources/main.js
+++ sources/main.js
@@ -1,10 +1,57 @@
-/************************************************************************************
- * Main file, with core function like init and gameloop
- ***********************************************************************************/
-
 window.onload = init;
 
+//looping function
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+
+/************************************************************************************
+ * init function
+ ***********************************************************************************/
 function init() 
 {
-	
+	canvasInstanciation();
+
+	//Instanciate Puppet context with all the systems we use
+	new Puppets(["RenderShape"]);
+
+	//Instanciate background entity
+	Puppets.Entities.createEntity(
+		entitiesModels["rect"], 
+		{ 
+			renderShape : {"color" : "black"},
+			size2d : 
+			{
+				"width"  : canvas.width,
+				"height" : canvas.height
+			}
+		}
+	);
+
+	gameloop();
 }
+
+
+/************************************************************************************
+ * instanciate the canvas, his context and hi size
+ ***********************************************************************************/
+function canvasInstanciation()
+{
+	window.canvas  = document.getElementById("canvas");
+	window.context = canvas.getContext("2d");
+	canvas.width   = 1024;
+	canvas.height  = 768;
+}
+
+function gameloop()
+{
+	Puppets.run();
+	requestAnimFrame(gameloop);
+}
+
