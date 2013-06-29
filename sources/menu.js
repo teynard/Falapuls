cfc039bcaf6bdad0b47b7bad2e64ea7aca5f1325
Gregoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2013-06-29 16:39:27 +0200

    Old Menu
    A mettre sur Master

--- sources/menu.js
+++ sources/menu.js
@@ -3,15 +3,15 @@ $(title).animate({fontSize:"100"}, 2000, 'easeOutElastic');
 $(playButton).toggle("puff");
 $(playButton).click(function(){
 	$(playButton).toggle("puff");
-	$(title).fadeOut(500, function ()
+	/*$(title).fadeOut(500, function ()
 	{
 		$(player1Select).animate({marginLeft:"0", marginTop:"0"}, 400, 'linear');
 		$(player2Select).animate({marginLeft:"512", marginTop:"0"}, 400, 'linear');
 		$(player3Select).animate({marginLeft:"0", marginTop:"384"}, 400, 'linear');
 		$(player4Select).animate({marginLeft:"512", marginTop:"384"}, 400, 'linear');
-	});
+	});*/
 	$(credits).fadeOut(500);
-	/*canvas.style.display="inline";
+	canvas.style.display="inline";
 	$(menu).fadeOut(1000);
-	init();*/
+	init();
 })
