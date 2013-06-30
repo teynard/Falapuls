c1627554f98b1607b7534df989583a9bed954c81
Gregoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2013-06-30 08:15:54 +0200

    Menu
    Ca progresse.

--- sources/menu.js
+++ sources/menu.js
@@ -1,17 +1,17 @@
+window.onload = function() {
 $(credits).fadeIn(2000);
 $(title).animate({fontSize:"100"}, 2000, 'easeOutElastic');
 $(playButton).toggle("puff");
 $(playButton).click(function(){
 	$(playButton).toggle("puff");
-	/*$(title).fadeOut(500, function ()
+	$(title).fadeOut(500, function ()
 	{
-		$(player1Select).animate({marginLeft:"0", marginTop:"0"}, 400, 'linear');
-		$(player2Select).animate({marginLeft:"512", marginTop:"0"}, 400, 'linear');
-		$(player3Select).animate({marginLeft:"0", marginTop:"384"}, 400, 'linear');
-		$(player4Select).animate({marginLeft:"512", marginTop:"384"}, 400, 'linear');
-	});*/
+		$(playerSelect).toggle("explode", {pieces: 4});
+		$(playerSelect).animate({fontSize:"60"}, 2000, 'linear');
+	});
 	$(credits).fadeOut(500);
-	canvas.style.display="inline";
+	/*canvas.style.display="inline";
 	$(menu).fadeOut(1000);
-	init();
+	init();*/
 })
+}
